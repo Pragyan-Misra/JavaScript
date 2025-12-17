762d938622561408402485afcc07871e74205fdd
Pragyan Misra <pragyanmisra080625@gmail.com>
2025-12-18 00:14:08 +0530

    basic knowledge of API

---
+++ API.js
@@ -0,0 +1,19 @@
+// API is basically how we interact with the backend database.
+// Easlier xml structure was used but now we use JSON for data interchange.
+
+// JSON - JavaScript Object Notation
+
+// JSON Syntax- everything is in key:value pair and in string format.
+// {
+//   "name" : "Pragyan",
+//   "price" : 1999,
+//   "coursename" : "JavaScript"
+// }
+
+// In JSON, keys must be strings and values must be a valid JSON data type (string, number, object, array, boolean, or null).
+
+// RandomUser API - a famous API to generate random user data for testing and prototyping.
+
+//we can either use prebuilt API or create our own API.
+
+// JSON formatter is a tool that helps to format and beautify JSON data for better readability.
