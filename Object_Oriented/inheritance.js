1a48c24e4df944818b01d8328d3d1284c23f493f
Pragyan Misra <pragyanmisra080625@gmail.com>
2026-01-03 21:52:38 +0530

    concept of inheritance

--- Object_Oriented/inheritance.js
+++ Object_Oriented/inheritance.js
@@ -7,3 +7,24 @@ class User{
     console.log(`Username is ${this.username}`);
   }
 }
+
+class Teacher extends User{
+  constructor(username, email, password){
+    super(username)
+    this.email = email
+    this.password = password
+  }
+  addCourse(){
+    console.log(`A new course was added by teacher by ${this.username}`)
+  }
+}
+
+const one = new Teacher('Pragyan', '1323@dghg','123345')
+
+one.addCourse()
+one.logMe();
+
+const two = new User("misra")
+two.logMe()
+
+console.log(one instanceof Teacher)
